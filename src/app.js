2c4015500718e40ed75b720c4129e6870f108c43
Simo Ngquseka <simo@swssr.co>
2020-11-19 20:06:31 +0200

    Add menu items

--- src/app.js
+++ src/app.js
@@ -1,27 +1,44 @@
-const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const middlewares = require('./middlewares');
-const api = require('./api');
+const middlewares = require("./middlewares");
+const api = require("./api");
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄'
+    message: "Hello from server!",
   });
 });
+app.get("/menu", (req, res) => {
+  const menu = [
+    {
+      name: "Call Me Brunch",
+      img:
+        "https://res.cloudinary.com/swssr/image/upload/v1604919303/tablefor/trq7ue8esyflkcbvozfj.png",
+      description: "Golden toast with banana berries.",
+    },
+    {
+      name: "Grilled Fish, Veggies",
+      img:
+        "https://res.cloudinary.com/swssr/image/upload/v1604919460/tablefor/ggl6gzokjnyioyb8gm7n.png",
+      description: "Golden toast with banana berries.",
+    },
+  ];
+  res.json(menu);
+});
 
-app.use('/api/v1', api);
+app.use("/api/v1", api);
 
 app.use(middlewares.notFound);
 app.use(middlewares.errorHandler);
